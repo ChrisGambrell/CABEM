2ba7679ebab10d1b8f8496babca2c4ff67fff54a
Chris Gambrell <christopher@gambrell.info>
2022-03-10 16:33:51 -0500

    Change text to identify branch

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,7 +29,7 @@ def create_app(test_config=None):
 
         @app.route('/hello')
         def hello():
-            return 'hello, world!'
+            return 'hello, world! testing123'
 
         @app.route('/secret')
         @login_required
